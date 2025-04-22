83193902835c79f473c645140c788ad929806124
Álvaro Morales Perujo <alvaro.mperujo@babelgroup.com>
2025-04-22 11:35:32 +0200

    fix: Cambiados algunos nombres de carpetas

---
+++ src/main/java/com/babelgroup/proyecto-final/service/request/approval/rules/approved_rules/DebtLessThanMonthlyQuotaRule.java
@@ -0,0 +1,4 @@
+package com.helloworld.renting.service.request.approval.rules.approved_rules;
+
+public class DebtLessThanMonthlyQuotaRule extends com.helloworld.renting.service.request.approval.rules.approved_rules.ApprovedRule {
+}
---
+++ src/main/java/com/babelgroup/proyecto-final/service/request/approval/rules/approved_rules/NotInInternalDebtsRule.java
@@ -0,0 +1,4 @@
+package com.helloworld.renting.service.request.approval.rules.approved_rules;
+
+public class NotInInternalDebtsRule extends com.helloworld.renting.service.request.approval.rules.approved_rules.ApprovedRule {
+}
