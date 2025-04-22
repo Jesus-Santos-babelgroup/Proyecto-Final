3977fb161e8c7a4fa43553e7e2e5014a35c06aab
Álvaro Morales Perujo <alvaro.mperujo@babelgroup.com>
2025-04-22 11:39:07 +0200

    Revert "fix: Cambiados algunos nombres de carpetas"
    This reverts commit 83193902835c79f473c645140c788ad929806124.

--- src/main/java/com/babelgroup/proyecto-final/service/request/approval/rules/approved_rules/DebtLessThanMonthlyQuotaRule.java
+++
@@ -1,4 +0,0 @@
-package com.helloworld.renting.service.request.approval.rules.approved_rules;
-
-public class DebtLessThanMonthlyQuotaRule extends com.helloworld.renting.service.request.approval.rules.approved_rules.ApprovedRule {
-}
--- src/main/java/com/babelgroup/proyecto-final/service/request/approval/rules/approved_rules/ScoringBelow5Rule.java
+++
@@ -1,4 +0,0 @@
-package com.helloworld.renting.service.request.approval.rules.approved_rules;
-
-public class ScoringBelow5Rule extends com.helloworld.renting.service.request.approval.rules.approved_rules.ApprovedRule {
-}
