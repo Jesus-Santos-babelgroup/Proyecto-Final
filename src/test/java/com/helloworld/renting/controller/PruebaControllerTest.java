866ae1a761a6dabaaec5ccb0cc787359cbbd6244
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-04-22 14:04:30 +0200

    feat: Preparación de tests de prueba

---
+++ src/test/java/com/helloworld/renting/controller/PruebaControllerTest.java
@@ -0,0 +1,39 @@
+package com.helloworld.renting.controller;
+
+import com.helloworld.renting.service.PruebaService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class PruebaControllerTest {
+
+    @Mock
+    PruebaService pruebaService;
+
+    PruebaController sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new PruebaController(pruebaService);
+    }
+
+    @Test
+    void should_returnMessage_when_pruebaExitosa() {
+        // Given
+        String message = "Hola, MyBatis desde MariaDB";
+        when(pruebaService.obtenerMensaje()).thenReturn(message);
+
+        // When
+        String actualMessage = sut.prueba();
+
+        // Then
+        assertEquals(message, actualMessage);
+    }
+
+}
