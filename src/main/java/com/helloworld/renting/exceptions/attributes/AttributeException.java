58279e817af831b20675cb11c7f62821a3536276
Jesús Santos <jesus.santos@babelgroup.com>
2025-04-23 12:08:25 +0200

    feat: Árbol de excepciones añadido
    Las excepciones deben de heredar todas, directa o indirectamente de RentingException.java, para utilizar el log.

---
+++ src/main/java/com/helloworld/renting/exceptions/attributes/AttributeException.java
@@ -0,0 +1,9 @@
+package com.helloworld.renting.exceptions.attributes;
+
+import com.helloworld.renting.exceptions.RentingException;
+
+public class AttributeException extends RentingException {
+    public AttributeException(String message) {
+        super(message);
+    }
+}
