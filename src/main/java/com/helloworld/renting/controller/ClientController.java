da2d252a43d6721d311bcf5639a2a674fe3a793a
Álvaro Morales Perujo <alvaro.mperujo@babelgroup.com>
2025-04-23 10:07:07 +0200

    docs: definicionEndpoints

--- src/main/java/com/helloworld/renting/controller/ClientController.java
+++ src/main/java/com/helloworld/renting/controller/ClientController.java
@@ -1,4 +1,14 @@
 package com.helloworld.renting.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clients")
 public class ClientController {
+
+    public ClientController() {
+    }
+
+
 }
--- src/main/java/com/helloworld/renting/controller/RequestController.java
+++ src/main/java/com/helloworld/renting/controller/RequestController.java
@@ -1,4 +1,14 @@
 package com.helloworld.renting.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/requests")
 public class RequestController {
+
+    public RequestController() {
+    }
+
 }
