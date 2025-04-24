e347ffccdcce8327ab5bb08ff007a6c91ec8a020
JuanGCarballo <juan.gcarballo@babelgroup.com>
2025-04-24 13:42:52 +0200

    feat: complete Rules interface

--- src/main/java/com/helloworld/renting/service/request/approval/Rules.java
+++ src/main/java/com/helloworld/renting/service/request/approval/Rules.java
@@ -1,4 +1,9 @@
 package com.helloworld.renting.service.request.approval;
 
+import com.helloworld.renting.entities.Client;
+import com.helloworld.renting.entities.RentingRequest;
+
 public interface Rules {
+
+    public boolean conditionMet(RentingRequest rentingRequest);
 }
