b09fe2a69bcfa901efd002005308637ace524197
JuanGCarballo <juan.gcarballo@babelgroup.com>
2025-04-24 13:53:27 +0200

    feat: añadir implementacion en proceso de deuda menor que tasa menor del mes

--- src/main/java/com/helloworld/renting/service/request/approval/rules/approved/DebtLessThanMonthlyQuotaRule.java
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/approved/DebtLessThanMonthlyQuotaRule.java
@@ -1,4 +1,21 @@
 package com.helloworld.renting.service.request.approval.rules.approved;
 
-public class DebtLessThanMonthlyQuotaRule extends ApprovedRule {
+import com.helloworld.renting.entities.Client;
+import com.helloworld.renting.entities.RentingRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DebtLessThanMonthlyQuotaRule extends ApprovedRule{
+
+    @Override
+    public boolean conditionMet(RentingRequest rentingRequest) {
+        Long idClient = rentingRequest.getClientId();
+        //TODO: obtener cliente real
+        Client client = new Client();
+        //TODO: obtener deuda real con client.getNif();
+        Double debt = 100.;
+        return debt < rentingRequest.getQuotaBaseMonthlyFee().doubleValue();
+    }
+
 }
