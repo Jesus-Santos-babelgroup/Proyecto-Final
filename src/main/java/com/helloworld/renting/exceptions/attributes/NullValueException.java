f1787963e48ec07d4c3007da0e0c50656b0265c1
Jesús Santos <jesus.santos@babelgroup.com>
2025-04-24 13:58:43 +0200

    NullValueException added

---
+++ src/main/java/com/helloworld/renting/exceptions/attributes/NullValueException.java
@@ -0,0 +1,7 @@
+package com.helloworld.renting.exceptions.attributes;
+
+public class NullValueException extends AttributeException {
+    public NullValueException(String message) {
+        super(message);
+    }
+}
