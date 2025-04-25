0cef5687df821a4a1d2c4d4c5ee0bc66249f745b
Jesús Santos <jesus.santos@babelgroup.com>
2025-04-25 13:16:50 +0200

    fix: Refactor in validate for Denial Rule NotInInternalDebtsRule

--- src/main/java/com/helloworld/renting/service/request/approval/rules/denial/NotInInternalDebtsRule.java
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/denial/NotInInternalDebtsRule.java
@@ -1,7 +1,10 @@
 package com.helloworld.renting.service.request.approval.rules.denial;
 
 import com.helloworld.renting.dto.RulesContextDto;
+import com.helloworld.renting.exceptions.attributes.InvalidRulesContextDtoException;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Component
 public class NotInInternalDebtsRule extends DenialRule {
@@ -14,5 +17,14 @@ public class NotInInternalDebtsRule extends DenialRule {
     @Override
     public String getName() {
         return "Holder is not in internal debts";
+    }
+
+    private void validate(RulesContextDto rulesContextDto) {
+        Objects.requireNonNull(rulesContextDto, "RulesContextDto no puede ser null");
+        if (rulesContextDto.getRequestId() == null) {
+            throw new InvalidRulesContextDtoException("RequestId es obligatorios");
+        } else if (rulesContextDto.getClientNif().isEmpty()) {
+            throw new InvalidRulesContextDtoException("ClientNif no puede ser vacio");
+        }
     }
 }
