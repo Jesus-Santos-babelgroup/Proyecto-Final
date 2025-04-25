c403c694ba78df724fbdba57e17711162df186af
alberto.carmona <alberto.carmona@babelgroup.com>
2025-04-25 11:44:35 +0200

    Añadir lógica regla 9

--- src/main/java/com/helloworld/renting/service/request/approval/rules/approved/ScoringBelow5Rule.java
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/approved/ScoringBelow5Rule.java
@@ -1,4 +1,16 @@
 package com.helloworld.renting.service.request.approval.rules.approved;
 
+import com.helloworld.renting.dto.RulesContextDto;
+
 public class ScoringBelow5Rule extends ApprovedRule {
+
+    @Override
+    public boolean conditionMet(RulesContextDto rulesContextDto) {
+        return rulesContextDto.getClientScoring()<5;
+    }
+
+    @Override
+    public String getName() {
+        return "scoringBelow5Rule";
+    }
 }
