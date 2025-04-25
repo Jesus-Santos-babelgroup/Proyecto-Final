43476e0f3ff0c375d4a0d1a5f6030e818d019adb
Marcos Morales Márquez <marcos.morales@babelgroup.com>
2025-04-25 12:29:52 +0200

    feat: test para la regla de aprobacion 5

---
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/approved/TotalInvestmentSmallerThan80k.java
@@ -0,0 +1,31 @@
+package com.helloworld.renting.service.request.approval.rules.approved;
+
+import com.helloworld.renting.dto.DebtDto;
+import com.helloworld.renting.dto.RulesContextDto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class TotalInvestmentSmallerThan80k extends ApprovedRule {
+
+    @Override
+    public boolean conditionMet(RulesContextDto rulesContextDto) {
+
+        BigDecimal totalInvestment = rulesContextDto.getTotalInvestment();
+        BigDecimal limit = BigDecimal.valueOf(80000);
+
+        if (totalInvestment == null) {
+            throw new IllegalArgumentException("Total Investment cannot be null");
+        }
+        if (totalInvestment.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Total Investment cannot be negative");
+        }
+        return (totalInvestment.compareTo(limit) < 0);
+    }
+
+    @Override
+    public String getName() {
+        return "TotalInvestmentSmallerThan80k";
+    }
+
+}
