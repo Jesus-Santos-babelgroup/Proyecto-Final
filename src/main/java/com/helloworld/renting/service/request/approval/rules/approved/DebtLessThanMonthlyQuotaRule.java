268b08bbf62d29bd9e6cc977a0326b3c0a677639
JuanGCarballo <juan.gcarballo@babelgroup.com>
2025-04-25 11:31:44 +0200

    fix: actualizar metodo para usar RulesContextDTO

--- src/main/java/com/helloworld/renting/service/request/approval/rules/approved/DebtLessThanMonthlyQuotaRule.java
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/approved/DebtLessThanMonthlyQuotaRule.java
@@ -1,21 +1,22 @@
 package com.helloworld.renting.service.request.approval.rules.approved;
 
-import com.helloworld.renting.entities.Client;
-import com.helloworld.renting.entities.RentingRequest;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.helloworld.renting.dto.RulesContextDto;
 
 public class DebtLessThanMonthlyQuotaRule extends ApprovedRule{
 
     @Override
-    public boolean conditionMet(RentingRequest rentingRequest) {
-        Long idClient = rentingRequest.getClientId();
-        //TODO: obtener cliente real
-        Client client = new Client();
-        //TODO: obtener deuda real con client.getNif();
-        Double debt = 100.;
-        return debt < rentingRequest.getQuotaBaseMonthlyFee().doubleValue();
+    public boolean conditionMet(RulesContextDto rulesContextDto) {
+        Double debt = rulesContextDto.getDebts()
+                .stream()
+                .map(debts -> debts.getAmount().doubleValue())
+                .reduce(0.0, Double::sum);
+        Double monthly_quota = rulesContextDto.getMonthly_quota();
+        return debt < monthly_quota;
+    }
+
+    @Override
+    public String getName() {
+        return "DebtLessThanMonthlyQuota";
     }
 
 }
