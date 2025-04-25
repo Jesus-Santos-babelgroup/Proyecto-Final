d2fef268b5e84edd8bdf9e5b6743d761f2a3e764
Jesús Santos <jesus.santos@babelgroup.com>
2025-04-25 10:48:10 +0200

    feat: Rules Context DTO added

---
+++ src/main/java/com/helloworld/renting/dto/RulesContextDto.java
@@ -0,0 +1,29 @@
+package com.helloworld.renting.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class RulesContextDto {
+    private Long requestId;
+    private BigDecimal totalInvestment;
+    private Integer monthsHired;
+    private LocalDate contractingDate;
+    private LocalDate startDate;
+    private String clientNif;
+    private LocalDate clientBirthDate;
+    private String clientNationality;
+    private Integer clientScoring;
+    private LocalDate employmentStartDate;
+    private BigDecimal netIncomeEmployed;
+    private BigDecimal netIncomeSelfEmployed;
+    private BigDecimal grossIncomeSelfEmployed;
+    private List<DebtDto> debts;
+    private List<NonpaymentDto> nonpayments;
+    private List<InformaDto> informaRecords;
+    private boolean previouslyRejected;
+    private boolean previouslyApprovedWithGuarantee;
+}
