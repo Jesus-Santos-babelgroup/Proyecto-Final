5bb07409400d7ca3a74e45dec02706c9d15196bc
alberto.carmona <alberto.carmona@babelgroup.com>
2025-04-25 12:37:11 +0200

    Añadir propiedades de rango del scoring

---
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/approved/scoringBelowThresholdRule/ScoringBelowThresholdProperties.java
@@ -0,0 +1,36 @@
+package com.helloworld.renting.service.request.approval.rules.approved.scoringBelowThresholdRule;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "scoringThreshold")
+public class ScoringBelowThresholdProperties {
+    private int scoringThreshold;
+    private int minScoring;
+    private int maxScoring;
+
+    public int getMaxScoring() {
+        return maxScoring;
+    }
+
+    public void setMaxScoring(int maxScoring) {
+        this.maxScoring = maxScoring;
+    }
+
+    public int getMinScoring() {
+        return minScoring;
+    }
+
+    public void setMinScoring(int minScoring) {
+        this.minScoring = minScoring;
+    }
+
+    public int getScoringThreshold() {
+        return scoringThreshold;
+    }
+
+    public void setScoringThreshold(int scoringThreshold) {
+        this.scoringThreshold = scoringThreshold;
+    }
+}
