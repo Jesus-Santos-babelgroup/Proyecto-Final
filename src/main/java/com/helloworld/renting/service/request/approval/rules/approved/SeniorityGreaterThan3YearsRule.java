1f2f913e7f139ff149cc911f2ba09f4053431d72
JuanGCarballo <juan.gcarballo@babelgroup.com>
2025-04-25 12:17:19 +0200

    test: testear regla de antiguedad de mas de 3 años

--- src/main/java/com/helloworld/renting/service/request/approval/rules/approved/SeniorityGreaterThan3YearsRule.java
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/approved/SeniorityGreaterThan3YearsRule.java
@@ -3,12 +3,12 @@ package com.helloworld.renting.service.request.approval.rules.approved;
 import com.helloworld.renting.dto.RulesContextDto;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
-
 public class SeniorityGreaterThan3YearsRule extends ApprovedRule{
     @Override
     public boolean conditionMet(RulesContextDto rulesContextDto) {
         int seniority = LocalDate.now().getYear() - rulesContextDto.getEmploymentStartDate().getYear();
+        if(seniority < 0)
+            throw new IllegalArgumentException("Employment start date cannot be in the future");
         return seniority <= 3;
     }
 
