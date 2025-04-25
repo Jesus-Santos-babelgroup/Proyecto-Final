4996a162beeb020c58cf469f336de1b14e4b1156
Jesús Santos <jesus.santos@babelgroup.com>
2025-04-25 10:49:08 +0200

    feat: Rules interface defined

--- src/main/java/com/helloworld/renting/service/request/approval/Rules.java
+++ src/main/java/com/helloworld/renting/service/request/approval/Rules.java
@@ -1,4 +1,7 @@
 package com.helloworld.renting.service.request.approval;
 
+import com.helloworld.renting.dto.RulesContextDto;
+
 public interface Rules {
+    boolean conditionMet(RulesContextDto rulesContextDto);
 }
