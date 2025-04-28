9b991db259902c40e9377af4c248ae9ceb446ab2
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-04-28 11:04:48 +0200

    feat: Mapper de datos bancarios implementado

--- src/main/java/com/helloworld/renting/mapper/EconomicDataMapper.java
+++ src/main/java/com/helloworld/renting/mapper/EconomicDataMapper.java
@@ -1,4 +1,41 @@
 package com.helloworld.renting.mapper;
 
+import com.helloworld.renting.entities.EconomicDataEmployed;
+import com.helloworld.renting.entities.EconomicDataSelfEmployed;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
 public interface EconomicDataMapper {
+
+    @Select("SELECT ede.ID_employee_data, ede.ID_client, ede.CIF, ede.Gross_income, ede.Start_date, ede.End_date, ede.Year_entry, ede.Net_income " +
+            "FROM Economic_data_employed ede WHERE ede.ID_client = #{clientId}")
+    @Results({
+            @Result(property = "id", column = "ID_employee_data"),
+            @Result(property = "clientId", column = "ID_client"),
+            @Result(property = "cif", column = "CIF"),
+            @Result(property = "grossIncome", column = "Gross_income"),
+            @Result(property = "startDate", column = "Start_date"),
+            @Result(property = "endDate", column = "End_date"),
+            @Result(property = "yearEntry", column = "Year_entry"),
+            @Result(property = "netIncome", column = "Net_income")})
+    List<EconomicDataEmployed> getEconomicDataEmployedByClientId(Long clientId);
+
+    @Select("SELECT ede.ID_autonomous_data, ede.ID_client, ede.Gross_income, ede.Net_income, ede.Year_entry " +
+            "FROM Economic_data_self_employed ede WHERE ede.ID_client = #{clientId}")
+    @Results({
+            @Result(property = "id", column = "ID_employee_data"),
+            @Result(property = "clientId", column = "ID_client"),
+            @Result(property = "grossIncome", column = "Gross_income"),
+            @Result(property = "netIncome", column = "Net_income"),
+            @Result(property = "yearEntry", column = "Year_entry")})
+    List<EconomicDataSelfEmployed> getEconomicDataSelfEmployedByClientId(Long clientId);
+
+    @Delete("DELETE FROM Economic_data_employed WHERE ID_client = #{clientId}")
+    void deleteEconomicDataEmployedByClientId(Long clientId);
+
+    @Delete("DELETE FROM Economic_data_self_employed WHERE ID_client = #{clientId}")
+    void deleteEconomicDataSelfEmployedByClientId(Long clientId);
+
 }
