03c66ced8a2493003ab1c30110118372d77546b9
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-04-28 10:29:57 +0200

    feat: Añadido EconomicDataNotFoundException

---
+++ src/main/java/com/helloworld/renting/exceptions/notfound/EconomicDataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.helloworld.renting.exceptions.notfound;
+
+public class EconomicDataNotFoundException extends NotFoundException {
+    public EconomicDataNotFoundException(String message) {
+        super(message);
+    }
+}
