d94384ad65f832e539ac2b592252125609f43ef1
Jesús Santos <jesus.santos@babelgroup.com>
2025-04-28 10:33:40 +0200

    feat: Tests for Denial Rule DebtAmountLessThanMonthlyQuote implemented

--- src/test/java/com/helloworld/renting/service/request/approval/rules/denial/DenialRuleDebtAmountLessThanMonthlyQuoteTest.java
+++ src/test/java/com/helloworld/renting/service/request/approval/rules/denial/DenialRuleDebtAmountLessThanMonthlyQuoteTest.java
@@ -1,12 +1,20 @@
 package com.helloworld.renting.service.request.approval.rules.denial;
 
+import com.helloworld.renting.dto.DebtDto;
 import com.helloworld.renting.dto.RulesContextDto;
+import com.helloworld.renting.exceptions.attributes.InvalidRulesContextDtoException;
 import com.helloworld.renting.service.request.approval.Rules;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DenialRuleDebtAmountLessThanMonthlyQuoteTest {
@@ -23,118 +31,85 @@ class DenialRuleDebtAmountLessThanMonthlyQuoteTest {
     @Test
     void should_returnTrue_when_debtIsLessThanMonthlyQuote() {
         // Given
+        DebtDto debt = new DebtDto();
+        debt.setId(1L);
+        debt.setAmount(BigDecimal.valueOf(100));
+        when(rulesContext.getMonthly_quota()).thenReturn(800.0);
+        when(rulesContext.getDebts()).thenReturn(List.of(debt));
 
         // When
+        boolean ruleState = sut.conditionMet(rulesContext);
 
         // Then
+        assertTrue(ruleState);
     }
 
     @Test
     void should_returnFalse_when_debtIsGreaterThanMonthlyQuote() {
         // Given
+        DebtDto debt = new DebtDto();
+        debt.setId(1L);
+        debt.setAmount(BigDecimal.valueOf(800));
+        when(rulesContext.getMonthly_quota()).thenReturn(100.0);
+        when(rulesContext.getDebts()).thenReturn(List.of(debt));
 
         // When
+        boolean ruleState = sut.conditionMet(rulesContext);
 
         // Then
+        assertFalse(ruleState);
+    }
+
+    @Test
+    void should_returnFalse_when_debtIsEqualToMonthlyQuote() {
+        // Given
+        DebtDto debt = new DebtDto();
+        debt.setId(1L);
+        debt.setAmount(BigDecimal.valueOf(100));
+        when(rulesContext.getMonthly_quota()).thenReturn(100.0);
+        when(rulesContext.getDebts()).thenReturn(List.of(debt));
+
+        // When
+        boolean ruleState = sut.conditionMet(rulesContext);
+
+        // Then
+        assertFalse(ruleState);
     }
 
     @Test
     void should_throwException_when_debtAmountIsNull() {
+        // Given
+        DebtDto debt = new DebtDto();
+        debt.setId(1L);
+        when(rulesContext.getMonthly_quota()).thenReturn(1000.0);
+        when(rulesContext.getDebts()).thenReturn(List.of(debt));
 
+        // when / Then
+        assertThrows(InvalidRulesContextDtoException.class, () -> sut.conditionMet(rulesContext));
     }
 
     @Test
     void should_throwException_when_debtAmountIsNegative() {
+        // Given
+        DebtDto debt = new DebtDto();
+        debt.setId(1L);
+        debt.setAmount(BigDecimal.valueOf(-500));
+        when(rulesContext.getMonthly_quota()).thenReturn(1000.0);
+        when(rulesContext.getDebts()).thenReturn(List.of(debt));
 
+        // when / Then
+        assertThrows(InvalidRulesContextDtoException.class, () -> sut.conditionMet(rulesContext));
     }
 
     @Test
-    void should_throwException_when_debtFormatIsInvalid() {
+    void should_throwException_when_monthlyQuotaIsNull() {
+        // Given
+        DebtDto debt = new DebtDto();
+        debt.setId(1L);
+        debt.setAmount(BigDecimal.valueOf(500));
+        when(rulesContext.getMonthly_quota()).thenReturn(null);
 
+        // When / Then
+        assertThrows(InvalidRulesContextDtoException.class, () -> sut.conditionMet(rulesContext));
     }
-
-//     @Mock
-//    DebtDto debt1;
-//
-//    @Mock
-//    DebtDto debt2;
-//
-//    @BeforeEach
-//    void setUp() {
-//        sut = new DenialRuleDebtAmountLessThanMonthlyQuote();
-//    }
-//
-//    @Test
-//    void should_returnTrue_when_debtIsGreaterThanMonthlyQuote() {
-//        // Given
-//        when(rulesContext.getMonthly_quota()).thenReturn(1000.0);
-//        when(debt1.getAmount()).thenReturn(new BigDecimal("800.00"));
-//        when(debt2.getAmount()).thenReturn(new BigDecimal("500.00"));
-//        when(rulesContext.getDebts()).thenReturn(List.of(debt1, debt2));
-//
-//        // When
-//        boolean ruleState = sut.conditionMet(rulesContext);
-//
-//        // Then
-//        assertTrue(ruleState);
-//    }
-//
-//    @Test
-//    void should_returnFalse_when_debtIsLessThanMonthlyQuote() {
-//        // Given
-//        when(rulesContext.getMonthly_quota()).thenReturn(2000.0);
-//        when(debt1.getAmount()).thenReturn(new BigDecimal("300.00"));
-//        when(debt2.getAmount()).thenReturn(new BigDecimal("400.00"));
-//        when(rulesContext.getDebts()).thenReturn(List.of(debt1, debt2));
-//
-//        // When
-//        boolean ruleState = sut.conditionMet(rulesContext);
-//
-//        // Then
-//        assertFalse(ruleState);
-//    }
-//
-//    @Test
-//    void should_returnFalse_when_debtsListIsEmpty() {
-//        // Given
-//        when(rulesContext.getMonthly_quota()).thenReturn(2000.0);
-//        when(rulesContext.getDebts()).thenReturn(List.of());
-//
-//        // When
-//        boolean ruleState = sut.conditionMet(rulesContext);
-//
-//        // Then
-//        assertFalse(ruleState); // empty debt list => not greater than quota
-//    }
-//
-//    @Test
-//    void should_throwException_when_debtAmountIsNull() {
-//        // Given
-//        when(rulesContext.getMonthly_quota()).thenReturn(1000.0);
-//        when(debt1.getAmount()).thenReturn(null);
-//        when(rulesContext.getDebts()).thenReturn(List.of(debt1));
-//
-//        // When / Then
-//        assertThrows(NullPointerException.class, () -> sut.conditionMet(rulesContext));
-//    }
-//
-//    @Test
-//    void should_throwException_when_monthlyQuotaIsNull() {
-//        // Given
-//        when(rulesContext.getMonthly_quota()).thenReturn(null);
-//        when(rulesContext.getDebts()).thenReturn(List.of());
-//
-//        // When / Then
-//        assertThrows(InvalidRulesContextDtoException.class, () -> sut.conditionMet(rulesContext));
-//    }
-//
-//    @Test
-//    void should_throwException_when_debtListIsNull() {
-//        // Given
-//        when(rulesContext.getMonthly_quota()).thenReturn(1200.0);
-//        when(rulesContext.getDebts()).thenReturn(null);
-//
-//        // When / Then
-//        assertThrows(InvalidRulesContextDtoException.class, () -> sut.conditionMet(rulesContext));
-//    }
 }
