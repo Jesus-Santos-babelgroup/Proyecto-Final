d339988873b964833fa842e35417548c87a55525
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-04-28 11:05:02 +0200

    feat: Servicio de borrado añadido

--- src/main/java/com/helloworld/renting/service/economicdata/EconomicDataService.java
+++ src/main/java/com/helloworld/renting/service/economicdata/EconomicDataService.java
@@ -1,5 +1,6 @@
 package com.helloworld.renting.service.economicdata;
 
+import com.helloworld.renting.exceptions.notfound.EconomicDataNotFoundException;
 import com.helloworld.renting.mapper.EconomicDataMapper;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,18 @@ public class EconomicDataService {
         this.economicDataMapper = economicDataMapper;
     }
 
-    public void deleteClient(Long id) {
-        
+    public void deleteEconomicDataFromClient(Long id) {
+        if (noEconomicDataExists(id)) {
+            throw new EconomicDataNotFoundException("No economic data found for client");
+        }
+
+        economicDataMapper.deleteEconomicDataEmployedByClientId(id);
+        economicDataMapper.deleteEconomicDataSelfEmployedByClientId(id);
+    }
+
+    private boolean noEconomicDataExists(Long id) {
+        return economicDataMapper.getEconomicDataEmployedByClientId(id).isEmpty() &&
+                economicDataMapper.getEconomicDataSelfEmployedByClientId(id).isEmpty();
     }
 
 }
