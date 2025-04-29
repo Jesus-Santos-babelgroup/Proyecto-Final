95511ac3cc9e86b8d33cdc9e7959c39e936325da
PabloMBabel <pablo.montero@babelgroup.com>
2025-04-29 12:14:21 +0200

    Actualizado ClientDto.java desde develop

--- src/main/java/com/helloworld/renting/dto/ClientDto.java
+++ src/main/java/com/helloworld/renting/dto/ClientDto.java
@@ -1,53 +1,46 @@
 package com.helloworld.renting.dto;
 
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.LocalDate;
+
+@Data
 public class ClientDto {
+
+    @Positive
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String firstSurname;
+
     private String secondSurname;
-    private String phone;
-    private String nif;
-    private String dateOfBirth;
+
+    @Email
     private String email;
+
+    @NotBlank
+    private String nif;
+
+    private String phone;
+
+    private LocalDate dateOfBirth;
+
     private Integer scoring;
+
+    @NotBlank
+    private String countryId;
+
+    @NotNull
+    @Positive
     private Long addressId;
-    private Long countryId;
+
+    @Positive
     private Long notificationAddressId;
-
-    // Getters y Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getFirstSurname() { return firstSurname; }
-    public void setFirstSurname(String firstSurname) { this.firstSurname = firstSurname; }
-
-    public String getSecondSurname() { return secondSurname; }
-    public void setSecondSurname(String secondSurname) { this.secondSurname = secondSurname; }
-
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
-
-    public String getNif() { return nif; }
-    public void setNif(String nif) { this.nif = nif; }
-
-    public String getDateOfBirth() { return dateOfBirth; }
-    public void setDateOfBirth(String dateOfBirth) { this.dateOfBirth = dateOfBirth; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public Integer getScoring() { return scoring; }
-    public void setScoring(Integer scoring) { this.scoring = scoring; }
-
-    public Long getAddressId() { return addressId; }
-    public void setAddressId(Long addressId) { this.addressId = addressId; }
-
-    public Long getCountryId() { return countryId; }
-    public void setCountryId(Long countryId) { this.countryId = countryId; }
-
-    public Long getNotificationAddressId() { return notificationAddressId; }
-    public void setNotificationAddressId(Long notificationAddressId) { this.notificationAddressId = notificationAddressId; }
 }
