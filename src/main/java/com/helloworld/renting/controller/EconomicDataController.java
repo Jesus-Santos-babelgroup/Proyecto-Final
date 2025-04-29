97d22343674db1f6f5a22cf85e0c289c4c67b06f
JuanGCarballo <juan.gcarballo@babelgroup.com>
2025-04-29 11:57:29 +0200

    feat: añadir controller para crear datos economico employed

--- src/main/java/com/helloworld/renting/controller/EconomicDataController.java
+++ src/main/java/com/helloworld/renting/controller/EconomicDataController.java
@@ -1,6 +1,5 @@
 package com.helloworld.renting.controller;
 
-import com.helloworld.renting.dto.ClientDto;
 import com.helloworld.renting.dto.EconomicDataEmployedDto;
 import com.helloworld.renting.dto.EconomicDataSelfEmployedDto;
 import com.helloworld.renting.service.client.EconomicDataService;
@@ -9,13 +8,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/clients/economic_data")
+@RequestMapping("/api/clients/")
 @Tag(name = "clients_economic_data", description = "Operaciones sobre la información bancaria de los clientes")
 public class EconomicDataController{
 
@@ -25,7 +21,7 @@ public class EconomicDataController{
         this.economicDataService = economicDataService;
     }
 
-    @PostMapping("/self_employee")
+    @PostMapping("{clientId}/self_employed")
     @Operation(
             summary = "Añadir información bancaria a un cliente",
             description = "Crea un nuevo cliente potencial",
@@ -34,8 +30,31 @@ public class EconomicDataController{
                     @ApiResponse(responseCode = "201", description = "Cliente creado")
             }
     )
-    public ResponseEntity<EconomicDataSelfEmployedDto> createClient(@Valid @RequestBody EconomicDataSelfEmployedDto economicDataSelfEmployedDto) {
-        EconomicDataSelfEmployedDto economicDataCreated = economicDataService.addEconomicData(economicDataSelfEmployedDto);
+    public ResponseEntity<EconomicDataSelfEmployedDto> addEconomicalDataSelfEmployee(
+            @Valid @RequestBody EconomicDataSelfEmployedDto economicDataSelfEmployedDto,
+            @PathVariable Long clientId) {
+
+        EconomicDataSelfEmployedDto economicDataCreated = economicDataService.addEconomicDataSelfEmployed(
+                economicDataSelfEmployedDto,
+                clientId);
+        return null;
+    }
+
+    @PostMapping("{clientId}/employed")
+    @Operation(
+            summary = "Añadir información bancaria a un cliente",
+            description = "Crea un nuevo cliente potencial",
+            tags = {"clients_economic_data"},
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Cliente creado")
+            }
+    )
+    public ResponseEntity<EconomicDataEmployedDto> addEconomicalDataEmployee(
+            @Valid @RequestBody EconomicDataEmployedDto economicDataEmployedDto,
+            @PathVariable Long clientId) {
+        EconomicDataEmployedDto economicDataCreated = economicDataService.addEconomicDataEmployed(
+                economicDataEmployedDto,
+                clientId);
         return null;
     }
 
