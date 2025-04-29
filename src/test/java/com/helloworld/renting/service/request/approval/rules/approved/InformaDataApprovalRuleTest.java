61a6d5b2d7e5f944e8d80b3e9271a67846e35463
JuanPedroGallardo <juanpedro.gallardo@babelgroup.com>
2025-04-29 09:17:22 +0200

    feat: Cabeceras tests

--- src/test/java/com/helloworld/renting/service/request/approval/rules/approved/InformaDataApprovalRuleTest.java
+++ src/test/java/com/helloworld/renting/service/request/approval/rules/approved/InformaDataApprovalRuleTest.java
@@ -1,4 +1,37 @@
 package com.helloworld.renting.service.request.approval.rules.approved;
 
+import org.junit.jupiter.api.Test;
+
 public class InformaDataApprovalRuleTest {
+
+    @Test
+    void testInformaDataApprovalRuleValidData() {
+
+    }
+
+    @Test
+    void testInformaDataApprovalRuleValidDataOlderThanTwoYears() {
+
+    }
+
+    @Test
+    void testInformaDataApprovalRuleSelfEmployedNoInforma() {
+
+    }
+
+    @Test
+    void testInformaDataApprovalRuleLessThan150k() {
+
+    }
+
+    @Test
+    void testInformaDataApprovalRuleNoInformaRecord() {
+
+    }
+
+    @Test
+    void testInformaDataApprovalRuleInvalidCIFFormat() {
+        
+    }
+
 }
