95f2e981d76209f1d13e556c8c2f852fb53bcaae
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-04-29 12:24:28 +0200

    fix: Cambia comprobación con el iso-a2

--- src/main/java/com/helloworld/renting/service/request/approval/rules/approved/SpanishNationalityRule.java
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/approved/SpanishNationalityRule.java
@@ -1,5 +1,6 @@
 package com.helloworld.renting.service.request.approval.rules.approved;
 
+import com.helloworld.renting.dto.RentingRequestDto;
 import com.helloworld.renting.exceptions.attributes.InvalidRulesContextDtoException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,15 +9,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpanishNationalityRule implements ApprovedRule {
 
+    private static final String SPAIN_ISOA2 = "ES";
     Logger logger = LoggerFactory.getLogger(SpanishNationalityRule.class);
 
-    public boolean conditionMet(RulesContextDto context) {
-        if (context.getClientNationality() == null) {
+    public boolean conditionMet(RentingRequestDto rentingRequestDto) {
+        String nationality = rentingRequestDto.getClient().getCountry().getIsoA2();
+
+        if (nationality == null) {
             logger.warn("Client nationality is null");
             throw new InvalidRulesContextDtoException("Client nationality is null");
         } else {
             logger.debug("SpanishNationalityRule checked");
-            return context.getClientNationality().equalsIgnoreCase("Spain");
+            return nationality.equalsIgnoreCase(SPAIN_ISOA2);
         }
     }
 
