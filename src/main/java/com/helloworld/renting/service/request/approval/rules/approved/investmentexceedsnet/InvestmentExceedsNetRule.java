2070610a02628c77942ce4f61737ddb20efa826e
oscargailb <oscar.gail@babelgroup.com>
2025-04-29 13:45:22 +0200

    fix: control de excepciones regla 1 y nueva excepcion para mapper

--- src/main/java/com/helloworld/renting/service/request/approval/rules/approved/investmentexceedsnet/InvestmentExceedsNetRule.java
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/approved/investmentexceedsnet/InvestmentExceedsNetRule.java
@@ -4,6 +4,8 @@ package com.helloworld.renting.service.request.approval.rules.approved.investmen
 import com.helloworld.renting.dto.ClientDto;
 import com.helloworld.renting.dto.RentingRequestDto;
 import com.helloworld.renting.entities.EconomicDataEmployed;
+import com.helloworld.renting.exceptions.attributes.InvalidMappedDataException;
+import com.helloworld.renting.exceptions.attributes.InvalidRulesContextDtoException;
 import com.helloworld.renting.service.request.approval.rules.approved.ApprovedRule;
 import org.springframework.stereotype.Component;
 
@@ -22,9 +24,31 @@ public class InvestmentExceedsNetRule implements ApprovedRule {
     public boolean conditionMet(RentingRequestDto rentingRequestDto) {
         BigDecimal netIncomeEmployed = mapper.getNetIncomeEmployed(rentingRequestDto.getId());
         BigDecimal netIncomeSelfEmployed = mapper.getNetIncomeSelfEmployed(rentingRequestDto.getId());
+        validateNetIncome(netIncomeEmployed, "Employed");
+        validateNetIncome(netIncomeSelfEmployed, "Self-employed");
         BigDecimal netIncome = netIncomeEmployed.add(netIncomeSelfEmployed);
         BigDecimal investment = rentingRequestDto.getQuotaFinal();
+        validateInvestment(investment);
         return investment.compareTo(netIncome) <= 0;
+    }
+
+    void validateInvestment(BigDecimal investment) {
+
+        if (investment == null) {
+            throw new InvalidRentingRequestDtoException("Investment cannot be null");
+        }
+        if (investment.compareTo(BigDecimal.ZERO) < 0) {
+            throw new InvalidRentingRequestDtoException("Investment cannot be negative");
+        }
+    }
+
+    void validateNetIncome(BigDecimal netIncome, String incomeType) {
+        if (netIncome == null) {
+            throw new InvalidMappedDataException(incomeType + " net income cannot be null");
+        }
+        if (netIncome.compareTo(BigDecimal.ZERO) < 0) {
+            throw new InvalidMappedDataException(incomeType + " net income cannot be negative");
+        }
     }
 
     @Override
