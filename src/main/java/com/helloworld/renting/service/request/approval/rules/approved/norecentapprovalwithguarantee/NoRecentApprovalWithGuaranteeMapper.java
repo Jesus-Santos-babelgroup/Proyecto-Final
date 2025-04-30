93378cd5ada5482a000a287c0b6cdc102f17e2f2
Laura Reyes <laura.reyes@babelgroup.com>
2025-04-30 12:10:31 +0200

    fix: refactorización regla 8

---
+++ src/main/java/com/helloworld/renting/service/request/approval/rules/approved/norecentapprovalwithguarantee/NoRecentApprovalWithGuaranteeMapper.java
@@ -0,0 +1,20 @@
+package com.helloworld.renting.service.request.approval.rules.approved.norecentapprovalwithguarantee;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDate;
+
+@Mapper
+public interface NoRecentApprovalWithGuaranteeMapper {
+
+    @Select("""
+        SELECT COUNT(*) 
+        FROM Renting_request
+        WHERE ID_client = #{clientId}
+          AND Final_result = 'APPROVED_WITH_WARRANTY'
+          AND Contracting_date >= #{fromDate}
+        """)
+    int countPreviousApprovalsWithGuarantees(@Param("clientId") Long clientId, @Param("fromDate") LocalDate fromDate);
+}
