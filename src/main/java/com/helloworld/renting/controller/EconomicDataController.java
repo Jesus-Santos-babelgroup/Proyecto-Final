d70777cb784e758c9439749db7c43f00146c89d0
Aitor Jiménez Segura <aitor.jimenez@babelgroup.com>
2025-04-30 10:27:16 +0200

    refactor: Ajuste de endpoints

--- src/main/java/com/helloworld/renting/controller/EconomicDataController.java
+++ src/main/java/com/helloworld/renting/controller/EconomicDataController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/economic-data")
+@RequestMapping("/api/clients")
 @Tag(name = "Economic data", description = "Operations with clients economic data")
 public class EconomicDataController {
 
@@ -24,7 +24,7 @@ public class EconomicDataController {
         this.economicDataService = economicDataService;
     }
 
-    @DeleteMapping("/clients/{id}")
+    @DeleteMapping("/{id}/economic-data/employed")
     @Operation(
             summary = "Deletes a client's economic data employed",
             description = "Deletes a client's economic data employed if it exists given the client's ID",
@@ -43,7 +43,7 @@ public class EconomicDataController {
         }
     }
 
-    @DeleteMapping("/clients/{id}")
+    @DeleteMapping("/{id}/economic-data/employed")
     @Operation(
             summary = "Deletes a client's economic data self employed",
             description = "Deletes a client's economic data self employed if it exists given the client's ID",
