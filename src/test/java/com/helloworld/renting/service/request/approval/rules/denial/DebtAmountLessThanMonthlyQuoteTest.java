3cc6fa55418cb1c1fe74bd5220498284374068fb
Álvaro Morales Perujo <alvaro.mperujo@babelgroup.com>
2025-05-02 11:53:25 +0200

    fix: Solucionados test de debtAmountLessThanMonthlyQuote

--- src/test/java/com/helloworld/renting/service/request/approval/rules/denial/DebtAmountLessThanMonthlyQuoteTest.java
+++ src/test/java/com/helloworld/renting/service/request/approval/rules/denial/DebtAmountLessThanMonthlyQuoteTest.java
@@ -1,5 +1,6 @@
 package com.helloworld.renting.service.request.approval.rules.denial;
 
+import com.helloworld.renting.dto.ClientDto;
 import com.helloworld.renting.dto.DebtDto;
 import com.helloworld.renting.dto.RentingRequestDto;
 import com.helloworld.renting.entities.Debt;
@@ -12,19 +13,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.mockito.quality.Strictness.LENIENT;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = LENIENT)
 class DebtAmountLessThanMonthlyQuoteTest {
 
     Rules sut;
+
     @Mock
     RentingRequestDto requestDto;
+    @Mock
+    ClientDto clientDto;
     @Mock
     DebtMapper debtMapper;
     @Mock
@@ -33,6 +41,16 @@ class DebtAmountLessThanMonthlyQuoteTest {
     @BeforeEach
     void setUp() {
         sut = new DebtAmountLessThanMonthlyQuote(debtMapper, mapStructDebt);
+
+        when(requestDto.getClient()).thenReturn(clientDto);
+        when(clientDto.getNif()).thenReturn("12345678A");
+        when(mapStructDebt.toDto(any(Debt.class))).thenAnswer(invocation -> {
+            Debt deuda = invocation.getArgument(0);
+            DebtDto dto = new DebtDto();
+            dto.setId(deuda.getId());
+            dto.setAmount(deuda.getAmount());
+            return dto;
+        });
     }
 
     @Test
@@ -42,7 +60,6 @@ class DebtAmountLessThanMonthlyQuoteTest {
         debt.setId(1L);
         debt.setAmount(BigDecimal.valueOf(100));
         when(requestDto.getQuotaFinal()).thenReturn(BigDecimal.valueOf(800.0));
-        when(requestDto.getClient().getNif()).thenReturn("12345678A");
         when(debtMapper.findDebtsByNif("12345678A")).thenReturn(List.of(debt));
 
         // When
@@ -59,7 +76,6 @@ class DebtAmountLessThanMonthlyQuoteTest {
         debt.setId(1L);
         debt.setAmount(BigDecimal.valueOf(800));
         when(requestDto.getQuotaFinal()).thenReturn(BigDecimal.valueOf(100.0));
-        when(requestDto.getClient().getNif()).thenReturn("12345678A");
         when(debtMapper.findDebtsByNif("12345678A")).thenReturn(List.of(debt));
 
         // When
@@ -76,7 +92,6 @@ class DebtAmountLessThanMonthlyQuoteTest {
         debt.setId(1L);
         debt.setAmount(BigDecimal.valueOf(100));
         when(requestDto.getQuotaFinal()).thenReturn(BigDecimal.valueOf(100.0));
-        when(requestDto.getClient().getNif()).thenReturn("12345678A");
         when(debtMapper.findDebtsByNif("12345678A")).thenReturn(List.of(debt));
 
         // When
@@ -91,12 +106,15 @@ class DebtAmountLessThanMonthlyQuoteTest {
         // Given
         Debt debt = new Debt();
         debt.setId(1L);
+        // debt.getAmount()==null
         when(requestDto.getQuotaFinal()).thenReturn(BigDecimal.valueOf(1000.0));
-        when(requestDto.getClient().getNif()).thenReturn("12345678A");
         when(debtMapper.findDebtsByNif("12345678A")).thenReturn(List.of(debt));
 
-        // when / Then
-        assertThrows(InvalidRentingRequestDtoException.class, () -> sut.conditionMet(requestDto));
+        // When / Then
+        assertThrows(
+                InvalidRentingRequestDtoException.class,
+                () -> sut.conditionMet(requestDto)
+        );
     }
 
     @Test
@@ -106,22 +124,24 @@ class DebtAmountLessThanMonthlyQuoteTest {
         debt.setId(1L);
         debt.setAmount(BigDecimal.valueOf(-500));
         when(requestDto.getQuotaFinal()).thenReturn(BigDecimal.valueOf(1000.0));
-        when(requestDto.getClient().getNif()).thenReturn("12345678A");
         when(debtMapper.findDebtsByNif("12345678A")).thenReturn(List.of(debt));
 
-        // when / Then
-        assertThrows(InvalidRentingRequestDtoException.class, () -> sut.conditionMet(requestDto));
+        // When / Then
+        assertThrows(
+                InvalidRentingRequestDtoException.class,
+                () -> sut.conditionMet(requestDto)
+        );
     }
 
     @Test
     void should_throwException_when_monthlyQuotaIsNull() {
         // Given
-        DebtDto debt = new DebtDto();
-        debt.setId(1L);
-        debt.setAmount(BigDecimal.valueOf(500));
         when(requestDto.getQuotaFinal()).thenReturn(null);
 
         // When / Then
-        assertThrows(InvalidRentingRequestDtoException.class, () -> sut.conditionMet(requestDto));
+        assertThrows(
+                InvalidRentingRequestDtoException.class,
+                () -> sut.conditionMet(requestDto)
+        );
     }
 }
